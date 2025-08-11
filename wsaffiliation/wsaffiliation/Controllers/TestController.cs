f534209fbfb7cc0ce9521bc4e5d32579439b6521
didier88-a <didier88.a@gmail.com>
2025-08-11 20:03:12 +0300

    add test controller

---
+++ wsaffiliation/wsaffiliation/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace wsaffiliation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("API is working!");
+        }
+    }
+}

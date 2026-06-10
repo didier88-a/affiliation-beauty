1a3bd78cf74cb63fb0cfffe46c698c4bc11d2399
didier88-a <didier88.a@gmail.com>
2026-06-10 12:25:29 +0300

    commit with cors shopify

--- wsaffiliation/wsaffiliation/Program.cs
+++ wsaffiliation/wsaffiliation/Program.cs
@@ -7,6 +7,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("ShopifyCors", policy =>
+    {
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,6 +31,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("ShopifyCors");
 
 app.MapControllers();
 

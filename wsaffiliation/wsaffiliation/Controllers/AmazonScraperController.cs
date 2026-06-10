07bb6fa0c9a96cc964f206a2299c7e1c4a9c36c8
didier88-a <didier88.a@gmail.com>
2026-06-10 12:02:55 +0300

    commit fix build2

--- wsaffiliation/wsaffiliation/Controllers/AmazonScraperController.cs
+++ wsaffiliation/wsaffiliation/Controllers/AmazonScraperController.cs
@@ -1,14 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HtmlAgilityPack;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
 using Microsoft.Playwright;
-using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Net;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace wsaffiliation.Controllers
 {
@@ -38,24 +39,24 @@ namespace wsaffiliation.Controllers
             var results = new List<AmazonProduct>();
 
             using var playwright = await Playwright.CreateAsync();
-
+            Console.WriteLine($"playwright : after");
             await using var browser = await playwright.Chromium.LaunchAsync(new()
             {
                 Headless = true
             });
-
+            Console.WriteLine($"playwright : after1");
             var page = await browser.NewPageAsync();
 
             var url = $"https://www.amazon.fr/s?k={Uri.EscapeDataString(query)}";
-
+            Console.WriteLine($"url : url");
             await page.GotoAsync(url);
             await page.WaitForSelectorAsync("[data-component-type='s-search-result']");
-
+            Console.WriteLine($"data-component-type");
             var html = await page.ContentAsync();
-
+            Console.WriteLine($"ContentAsync");
             var doc = new HtmlDocument();
             doc.LoadHtml(html);
-
+            Console.WriteLine($"LoadHtml");
             var nodes = doc.DocumentNode.SelectNodes("//div[@data-component-type='s-search-result']");
 
             if (nodes == null) return results;

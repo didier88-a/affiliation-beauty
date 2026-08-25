52a5eeb5b412237dabf99c0ce907e817d639735d
Moshe Attali <moshe-a@mgarisr.onmicrosoft.com>
2026-08-25 10:39:54 +0300

    Push Naya Controler

---
+++ wsaffiliation/wsaffiliation/Controllers/NayaController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace wsaffiliation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NayaController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public NayaController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("popular-guides")]
+        public async Task<IActionResult> GetPopularGuides()
+        {
+           
+            var supabaseUrl = Environment.GetEnvironmentVariable("SUPABASE_URL");
+            var supabaseKey = Environment.GetEnvironmentVariable("SUPABASE_KEY");
+            
+
+            var url =
+                $"{supabaseUrl}/rest/v1/rpc/get_popular_guides";
+
+            using var request = new HttpRequestMessage(
+                HttpMethod.Post,
+                url
+            );
+
+            request.Headers.Add(
+                "apikey",
+                supabaseKey
+            );
+
+            request.Headers.Authorization =
+                new AuthenticationHeaderValue(
+                    "Bearer",
+                    supabaseKey
+                );
+
+            using var httpClient = new HttpClient();
+
+            var response =
+                await httpClient.SendAsync(request);
+
+            var json =
+                await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return StatusCode(
+                    (int)response.StatusCode,
+                    json
+                );
+            }
+
+            return Content(
+                json,
+                "application/json"
+            );
+        }
+    }
+}
